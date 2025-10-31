e779711a4615aea545cfa9120243cede0ca6447c
MovieGoers <alscjf4104@gmail.com>
2025-10-31 16:49:16 +0900

    [FE] 불필요한 코드 제거

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Info from "./components/pages/Info";
