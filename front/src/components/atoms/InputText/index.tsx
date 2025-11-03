c68bcf75b80d22226d52c9e05c1f85b7e1a91aef
MovieGoers <alscjf4104@gmail.com>
2025-11-03 14:54:43 +0900

    [FE] 로그인 레이아웃 작성, 링크 추가

--- front/src/components/atoms/InputText/index.tsx
+++ front/src/components/atoms/InputText/index.tsx
@@ -1,24 +1,39 @@
-import styles from './InputText.module.scss';
-import classNames from 'classnames/bind';
+import styles from "./InputText.module.scss";
+import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles);
 
 interface InputTextProps {
-    id: string;
-    labelText: string;
-    placeholder: string;
-    className?: string;
+  id: string;
+  labelText: string;
+  placeholder: string;
+  className?: string;
+  type?: string;
+  isRequired?: boolean;
 }
 
-const InputText = ({ id, className, labelText, placeholder }: InputTextProps) => {
-    return (
-        <div className={cx('input_wrap', className)}>
-            <label htmlFor={id} className={cx('label', 'blind')}>
-                {labelText}
-            </label>
-            <input type="text" id={id} className={cx('input')} placeholder={placeholder} />
-        </div>
-    )
-}
+const InputText = ({
+  id,
+  className,
+  labelText,
+  placeholder,
+  type = "text",
+  isRequired = false,
+}: InputTextProps) => {
+  return (
+    <div className={cx("input_wrap", className)}>
+      <label htmlFor={id} className={cx("label", "blind")}>
+        {labelText}
+      </label>
+      <input
+        type={type}
+        id={id}
+        className={cx("input")}
+        placeholder={placeholder}
+        required={isRequired}
+      />
+    </div>
+  );
+};
 
 export default InputText;
--- front/src/components/atoms/LoginButton/index.tsx
+++ front/src/components/atoms/LoginButton/index.tsx
@@ -1,18 +1,18 @@
-import styles from './LoginButton.module.scss';
-import classNames from 'classnames/bind';
+import styles from "./LoginButton.module.scss";
+import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles);
 
 interface LoginButtonProps {
-    className?: string;
+  className?: string;
 }
 
 const LoginButton = ({ className }: LoginButtonProps) => {
-    return (
-        <button className={cx('button', className)}>
-            로그인
-        </button>
-    );
-}
+  return (
+    <button type="submit" className={cx("button", className)}>
+      로그인
+    </button>
+  );
+};
 
 export default LoginButton;
